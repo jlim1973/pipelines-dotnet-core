2935fea9733da570cb0bde3d4646efeb0eb520ba
jlim1973 <32504629+jlim1973@users.noreply.github.com>
2019-07-19 14:24:18 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,7 +14,6 @@ namespace pipelines_dotnet_core
     {
         public static void Main(string[] args)
         {
-            error
             CreateWebHostBuilder(args).Build().Run();
         }
 
